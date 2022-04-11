5034f2fafbe99c32738dd9bc41e27a2e6fdb99a2
jepra777 <jere.prasetyo.jp@gmail.com>
2022-04-11 17:38:08 +0700

    [FE]React Init + Install MUI

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import {Button} from '@mui/material/';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
